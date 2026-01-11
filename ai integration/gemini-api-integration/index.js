c9db8fe045704a170744cfa157d4908936a460ea
UnpolishedNoob <banikpritom321@gmail.com>
2026-01-12 01:05:52 +0600

    Tried to use gemini using javascript

---
+++ ai integration/gemini-api-integration/index.js
@@ -0,0 +1,33 @@
+// import { configDotenv } from "dotenv";
+// import { GoogleGenAI } from "@google/genai";
+
+// configDotenv();
+
+// const apiKey=process.env.GEMINI_API_KEY;
+
+// // The client gets the API key from the environment variable `GEMINI_API_KEY`.
+// const ai = new GoogleGenAI({apiKey});
+
+// async function main() {
+//   const response = await ai.models.generateContent({
+//     model: "gemini-2.5-flash",
+//     contents: "Explain how AI works in a few words",
+//   });
+//   console.log(response.text);
+// }
+
+// main();
+
+import { GoogleGenAI } from "@google/genai";
+
+const ai = new GoogleGenAI({ apiKey: "AIzaSyDp54uOFr0ufnzp43J_ncDep0E2Cc57vFc" });
+
+async function main() {
+  const response = await ai.models.generateContent({
+    model: "gemini-2.5-flash",
+    contents: "Explain about KUET,Bangladesh in few lines",
+  });
+  console.log(response.text);
+}
+
+main();
