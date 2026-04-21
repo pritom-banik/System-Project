1126dfd4a298d5f278351f15ef352807c0f2c746
UnpolishedNoob <banikpritom321@gmail.com>
2026-04-21 23:54:38 +0600

    designed a get api for verification of nginx

--- server/server.js
+++ server/server.js
@@ -31,7 +31,7 @@ function cosineSimilarity(a, b) {
 async function getEmbedding(title) {
   try {
     const response = await axios.post("http://localhost:11434/api/embeddings", {
-      model: "bge-m3:latest", 
+      model: "bge-m3:latest",
       prompt: title,
     });
     return response.data.embedding;
@@ -48,13 +48,13 @@ app.post("/ans", async (req, res) => {
   //console.log("Suggested title:", req.body);
   const { maintitle, title } = req.body;
   console.log("Maintitle:", maintitle);
-console.log("Title:", title);
+  console.log("Title:", title);
 
   try {
-    const mainEmbedding=await getEmbedding(maintitle)
+    const mainEmbedding = await getEmbedding(maintitle)
     const suggestedEmbedding = await getEmbedding(title);
     const score = cosineSimilarity(mainEmbedding, suggestedEmbedding);
-    console.log("Suggested title:", req.body," Similarity score:", score);
+    console.log("Suggested title:", req.body, " Similarity score:", score);
     const isValid = score >= 0.35;
 
     res.json({ Success: isValid, similarity: score });
@@ -64,6 +64,24 @@ console.log("Title:", title);
   }
 });
 
+app.get("/", (req, res) => {
+  res.send(`
+    <!DOCTYPE html>
+    <html>
+    <head>
+      <title>Focus Keeper API</title>
+    </head>
+    <body>
+      <div class="container" style="text-align:center; padding:40px; border-radius:12px; background:linear-gradient(135deg,#667eea,#764ba2); color:white; font-family:Arial, sans-serif; box-shadow:0 8px 20px rgba(0,0,0,0.2);">
+      <h1 style="margin-bottom:10px; font-size:2.5rem;"> Focus Keeper API</h1>
+      <p style="font-size:1.2rem; opacity:0.9;">Your productivity companion is up and running!</p>
+      <h2 style="margin-top:30px; font-size:1.5rem; color:yellow">Endpoints:${port}</h2>
+      </div>
+    </body>
+    </html>
+  `);
+});
+
 app.listen(port, () => {
-  console.log("Server started for api");
+  console.log("Focus Keeper app listening on port " + port);
 });
